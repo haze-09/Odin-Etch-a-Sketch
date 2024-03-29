f1380e7b4bcf1af9ca5d53cb617d55646aa50e48
haze-09 <68776764+haze-09@users.noreply.github.com>
2024-03-29 11:18:44 +0530

    greyscale functionality achieved

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 let sketchPad = document.querySelector('#sketchPad');
-let n = 16;
+let n = 64;
 
 for (let i=0;i<n;i++){
     let row = document.createElement('div');
     row.classList.add('row');
     sketchPad.appendChild(row);
-}
+};
 
-let rows= document.querySelectorAll('.row')
-console.log(rows)
+let rows= document.querySelectorAll('.row');
 
 
 for(row of rows){    
     for (let i=0;i<n;i++){  
-        let square = document.createElement('div');
+        var square = document.createElement('div');
         square.classList.add('square');      
         row.appendChild(square);
-    }
+    };
     
+};
+
+let pixels = document.querySelectorAll('.square');
+
+function incrementOpacity(){
+    const currOpacityString = this.style.getPropertyValue("--square_opacity");
+    const currOpacity = currOpacityString ? parseFloat(currOpacityString) : 0;
+    const opacity = Math.min(+currOpacity + 0.1, 1);    
+    this.style.setProperty("--square_opacity", opacity);
+}
+const greyscale=document.querySelector('#greyscale');
+const rainbow = document.querySelector('#rainbow');
+
+greyscale.addEventListener('click',()=>{
+    for(pixel of pixels){
+        pixel.style.setProperty("--square_color",'black');
+    }
+});
+
+rainbow.addEventListener('click',()=>{
+    for(pixel of pixels){
+        
+    }
+})
+
+for(pixel of pixels){
+    pixel.addEventListener('mouseenter',incrementOpacity);
 }
